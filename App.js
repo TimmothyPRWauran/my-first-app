45c94b5873b78b3f4abf6ff4454b1a247f6f136c
unknown <s2200123@student.unklab.ac.id>
2021-12-01 23:20:00 +0800

    Final

--- App.js
+++ App.js
@@ -1,6 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
 
+
+function App() {
+  return (
+    <div>
+      
+       <div>
+  <meta charSet="UTF-8" />
+  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="Appsheet" href="App.css" />
+  <title>My Website</title>
+  <img src="https://avatars.githubusercontent.com/u/85221854?s=400&u=297223521f2f96dd83293d2e954c595aed72f430&v=4" width={320} height={170} alt="IMG_4716.PNG" />
+  <hr />
+  <h3>INFORMASI PRIBADI</h3>
+  <div>
+    <p>Nama           : Timmothy Patrick Rafael Wauran</p>
+    <p>Gender         : Laki-Laki</p>
+    <p>Hobi           : Gaming</p>
+    <p>Tgl.Lahir      : 19/10/2002</p>
+    <p>Agama          : kristen Advent</p>
+    <label>Kota asal</label>
+    <br />
+    <select>
+      <option value="Manado">Manado</option>
+      <option value="Konoha">Tondano</option>
+      <option value="Tomohon">Tomohon</option>
+    </select>
+  </div>
+  <br />
+  <hr />
+  <h3>INFORMASI KONTAK</h3>
+  <p>Email Address  : <span style={{color: 'blue'}}><i>S2200123@student.unklab.ac.id</i></span></p>
+  <p>No.Telpon      : +6282296354302</p>
+  <hr />
+  <h3>Favorite Web</h3>
+  <p><a href="https://neonime.site/">Situs Favorite</a></p>
+  <hr />
+  <h3>TUJUAN SAAT INI</h3>
+  <p>Membanggakan Orang tua</p>
+  <hr />
+  <h3>Sekolah</h3>
+  <table>
+    <tbody><tr>
+        <td>SD Negeri 2 Dumoga</td>
+      </tr>
+      <tr>
+        <td>Smp Negeri 2 Dumoga</td>
+      </tr>
+      <tr>
+        <td>SMK Yadika Kopandakan II</td>
+      </tr>
+    </tbody></table>
+</div>
+
+    </div>
+  )
+}
+export default App;
+
+
+
+
+
+
+/*import Content from "./components/Content";
+import Footer from "./components/Footer";
+import HeaderClass from "./components/HeaderClass";
+import NavBar from "./components/NavBar";
+
+
+function App (){
+  return (
+    <div>
+      <HeaderClass greeting="nice to meet you"/>
+      <Footer/>
+      <NavBar/>
+      <Content/>
+
+    </div>
+  )
+}*/
+
+
+
+
+
+/*import logo from './logo.svg';
+import './App.css';*/
+
+/*const Hello = () => {
+ const name = "Timmothy Wauran"
+  return (
+    <>
+    <h1>Hello {name}</h1>
+    <h3>Nice gameplay<h3/>
+    </>
+  )
+}*/
+/*function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Hello/>
+       <Hello/>
+       <Hello name="Timmohty"/>
+      </header>
+    </div>
+  );
+}*/
+/*
 function App() {
   return (
     <div className="App">
@@ -20,6 +128,4 @@ function App() {
       </header>
     </div>
   );
-}
-
-export default App;
+}*/
